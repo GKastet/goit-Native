f13d6da5e9be4a8c4d0bc7717454a276c1d826bf
GKastet <127600582+GKastet@users.noreply.github.com>
2023-08-24 19:54:33 +0200

    background image

--- AwesomeProject/App.js
+++ AwesomeProject/App.js
@@ -1,11 +1,31 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { Button, Image, ImageBackground, ScrollView, StyleSheet, Text, TextInput, TextInputComponent, View } from 'react-native';
+import { useFonts } from 'expo-font';
+import { RegistrationScreen } from './Screens/RegistrationScreen';
+import { LoginScreen } from './Screens/LoginScreen';
+import { PostsScreen } from './Screens/PostsScreen';
 
 export default function App() {
+  // const [fontsLoaded] = useFonts({
+  //   'Inter-Black': require('./assets/fonts/Inter-Black.otf'),
+  // });
+  // if (!fontsLoaded) {
+  //   return null;
+  // }
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!123</Text>
+      <ImageBackground source={require('./img/PhotoBG.png')} resizeMode="cover" style={styles.image}>
+      <Text style={styles.text}>Open up App.js to start working on your app!</Text>
+      {/* <Image source={require('./img/PhotoBG.png')} style={{width: 500, height: 500}}/> */}
+      <RegistrationScreen />
+      {/* <LoginScreen/>
+      <PostsScreen/> */}
+      {/* <Image source={require('./img/PhotoBG.png')}/> */}
+      {/* <ImageBackground source={require('./img/PhotoBG.png')}/> */}
+      {/* <Button type='button'>Click</Button> */} 
       <StatusBar style="auto" />
+      </ImageBackground>
     </View>
   );
 }
@@ -17,4 +37,14 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
+  image: {
+    width: '100%',
+    height: '100%',    
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  text: {
+    color: '#fff',
+
+  }
 });
---
+++ AwesomeProject/Screens/RegistrationScreen.js
@@ -0,0 +1,26 @@
+import { Button, Image, ImageBackground, Text, TextInput, View } from "react-native";
+
+
+export const RegistrationScreen = () => {
+    
+  return (
+    <View>
+      {/* <ImageBackground source={require('../img/PhotoBG.png')} resizeMode="cover"> */}
+      
+      <Text>RegistrationScreen!</Text>
+      
+    {/* </ImageBackground> */}
+      {/* <Image source={require('../img/PhotoBG.png')}/> */}
+      {/* <ImageBackground source={require('../img/PhotoBG.png')}/> */}
+    </View>
+  );
+};
+
+// const styles = StyleSheet.create({
+//   container: {
+//     flex: 1,
+//     backgroundColor: "#fff",
+//     alignItems: "center",
+//     justifyContent: "center",
+//   },
+// });
